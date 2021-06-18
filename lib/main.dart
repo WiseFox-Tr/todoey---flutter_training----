2728a6dca68ca08d667adede863d83ce26b686f5
Maxime Traverse <maxime.Traverse@orange.fr>
2021-06-18 17:33:43 +0200

    add a MaterialApp & redirection to home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todoey_app/ui/screens/tasks_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,7 +8,9 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: TasksScreen(),
+    );
   }
 }
 
