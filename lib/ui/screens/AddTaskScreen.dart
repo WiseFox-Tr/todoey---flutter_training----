cb0b1c194959b4d17ace95a00564b2398f41f6c7
Maxime Traverse <maxime.Traverse@orange.fr>
2021-06-24 18:49:24 +0200

    use methods from task provider to allow adding a new task

--- lib/ui/screens/AddTaskScreen.dart
+++ lib/ui/screens/AddTaskScreen.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todoey_app/provider/TaskProvider.dart';
 
 class AddTaskScreen extends StatelessWidget {
-
-  final Function addATaskCallback;
-  final Function updateTaskInputCallback;
-
-  AddTaskScreen({@required this.addATaskCallback, @required this.updateTaskInputCallback});
 
   @override
   Widget build(BuildContext context) {
@@ -18,38 +15,45 @@ class AddTaskScreen extends StatelessWidget {
           color: Colors.white,
           borderRadius: BorderRadius.only(topLeft: Radius.circular(20.0), topRight: Radius.circular(20.0))
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              'Add Task',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.lightBlueAccent,
-                fontSize: 30.0,
-              ),
-            ),
-            TextField(
-              cursorColor: Colors.lightBlueAccent,
-              autofocus: true,
-              textAlign: TextAlign.center,
-              onChanged: updateTaskInputCallback,
-            ),
-            SizedBox(height: 20.0),
-            TextButton(
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.lightBlueAccent,
-              ),
-              onPressed: addATaskCallback,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'Add',
-                  style: TextStyle(color: Colors.white),
+        child: Consumer<TaskProvider>(
+          builder: (context, taskProvider, child) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  'Add Task',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.lightBlueAccent,
+                    fontSize: 30.0,
+                  ),
                 ),
-              ),
-            ),
-          ],
+                TextField(
+                  cursorColor: Colors.lightBlueAccent,
+                  autofocus: true,
+                  textAlign: TextAlign.center,
+                  onChanged: (newTask) => taskProvider.updateTaskInput(newTask),
+                ),
+                SizedBox(height: 20.0),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.lightBlueAccent,
+                  ),
+                  onPressed: () {
+                    taskProvider.addATask();
+                    Navigator.pop(context);
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Add',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
