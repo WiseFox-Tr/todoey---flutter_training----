4b2dd3c4e47df41dfd54a1f6eed96d044aadc63d
Maxime Traverse <maxime.Traverse@orange.fr>
2021-06-24 18:49:43 +0200

    use methods from task provider to allow to check tasks

--- lib/ui/widgets/task_list.dart
+++ lib/ui/widgets/task_list.dart
@@ -1,32 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:todoey_app/model/Task.dart';
+import 'package:todoey_app/provider/TaskProvider.dart';
 import 'package:todoey_app/ui/widgets/TaskTile.dart';
 
-class TaskList extends StatefulWidget {
+class TaskList extends StatelessWidget {
 
-  final List<Task> tasks;
-
-  TaskList({@required this.tasks});
-
-  @override
-  _TaskListState createState() => _TaskListState();
-}
-
-class _TaskListState extends State<TaskList> {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: widget.tasks.length,
-      itemBuilder: (context, index) {
-      return TaskTile(
-        taskName: widget.tasks[index].name,
-        isChecked: widget.tasks[index].isDone,
-        checkboxCallback: (newState) {
-          setState(() {
-            widget.tasks[index].toggleDone();
-          });
-        },
-      );
-    });
+    return Consumer<TaskProvider>(
+      builder: (context, taskProvider, child) {
+        return ListView.builder(
+          itemCount: taskProvider.tasksNumber,
+          itemBuilder: (context, index) {
+            final Task currentTask = taskProvider.tasks[index];
+            return TaskTile(
+              taskName: currentTask.name,
+              isChecked: currentTask.isDone,
+              checkboxCallback: (newState) => taskProvider.updateTask(currentTask),
+            );
+          },
+        );
+      },
+    );
   }
 }
